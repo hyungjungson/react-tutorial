5ee9fa1702247cb56e9c330aed51fd0699ad00ad
Hyung-Jung Son <thsgudwjd@nate.com>
2023-07-25 11:26:31 +0900

    2. `useState`를 활용하여 **임시 데이터(최소 3개)**를 만들고, **메인페이지**(`/`)에 보여줍니다.     - db.json은 아직 **활용하지 않습니다.**     - 게시물 데이터엔 다음과 같은 데이터가 존재해야 합니다.         - id - 고유한 아이디여야 합니다         - title - 제목입니다.         - content - 내용입니다.         - author - 작성자입니다. 추후에 firebase와 연결 후 이메일을 여기에 작성하겠습니다.     - **map**을 활용합니다. → map이 어떻게 동작하는지 설명할 줄 알아야 합니다. (주석으로 작성해놓으세요)     - **map** 내부에 **key값 관련 warning**이 발생하지 않도록 합니다.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../common/Header";
 import Container from "../common/Container";
 
 export default function Main() {
   const navigate = useNavigate();
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "제목입니다.",
+      content: "내용입니다.",
+      author:
+        "작성자입니다. 추후에 firebase와 연결 후 이메일을 여기에 작성하겠습니다.",
+    },
+  ]);
   return (
     <>
       <Header />
@@ -32,7 +41,92 @@ export default function Main() {
             추가
           </button>
         </div>
-        {[1, 2, 3, 4].map((item) => (
+        {/* useState를 활용하여 데이터 보여주고 map함수를 사용하여 리스트 보여주기 */}
+        {data.map((dataList) => {
+          return (
+            <div
+              key={dataList.id} // key값 관련 warning 방지
+              style={{
+                backgroundColor: "#EEEEEE",
+                height: "100px",
+                borderRadius: "24px",
+                marginBottom: "12px",
+                display: "flex",
+                padding: "12px 16px 12px 16px",
+              }}
+            >
+              <div
+                onClick={() => {
+                  navigate("/detail/1");
+                }}
+                style={{
+                  flex: 4,
+                  borderRight: "1px solid lightgrey",
+                  cursor: "pointer",
+                }}
+              >
+                <h2>{dataList.title}</h2>
+                <p
+                  style={{
+                    width: "300px",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {dataList.content}
+                </p>
+              </div>
+              <div
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "end",
+                  justifyContent: "space-around",
+                  gap: "12px",
+                }}
+              >
+                <div>{dataList.authors}</div>
+                <div>
+                  <button
+                    onClick={() => {
+                      navigate("/edit");
+                    }}
+                    style={{
+                      border: "none",
+                      padding: "8px",
+                      borderRadius: "6px",
+                      backgroundColor: "orange",
+                      color: "white",
+                      cursor: "pointer",
+                      marginRight: "6px",
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    onClick={() => {
+                      alert("삭제할까?");
+                    }}
+                    style={{
+                      border: "none",
+                      padding: "8px",
+                      borderRadius: "6px",
+                      backgroundColor: "red",
+                      color: "white",
+                      cursor: "pointer",
+                    }}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+        {/* {[1, 2, 3, 4].map((item) => (
           <div
             key={item}
             style={{
@@ -115,7 +209,7 @@ export default function Main() {
               </div>
             </div>
           </div>
-        ))}
+        ))} */}
       </Container>
     </>
   );
